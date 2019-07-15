f92a4a767ac8404ce7125a1fa27c0fd259b5729e
w1tcher <w1tcher@ubuntu>
2019-07-15 05:14:44 -0700

    add mem info

--- workspace-provider/src/main/java/com/aliware/tianchi/CallbackServiceImpl.java
+++ workspace-provider/src/main/java/com/aliware/tianchi/CallbackServiceImpl.java
@@ -18,6 +18,39 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 public class CallbackServiceImpl implements CallbackService {
 
+    public static long readFileInt(String fileName) {
+        File file = new File(fileName);
+        BufferedReader reader = null;
+        try {
+            // System.out.println("以行为单位读取文件内容，一次读一整行：");
+            reader = new BufferedReader(new FileReader(file));
+            String tempString = null;
+            // int line = 1;
+            // 一次读入一行，直到读入null为文件结束
+            // while ((tempString = reader.readLine()) != null) {
+            //     // 显示行号
+            //     System.out.println("line " + line + ": " + tempString);
+            //     line++;
+            // }
+            if ((tempString = reader.readLine()) == null){
+                return 0;
+            }
+            reader.close();
+            // System.out.println(tempString.length());
+            return Long.parseLong(tempString);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e1) {
+                }
+            }
+        }
+        return 0;
+    }
+
     public CallbackServiceImpl() {
         timer.schedule(new TimerTask() {
             @Override
@@ -25,7 +58,10 @@ public class CallbackServiceImpl implements CallbackService {
                 if (!listeners.isEmpty()) {
                     for (Map.Entry<String, CallbackListener> entry : listeners.entrySet()) {
                         try {
-                            entry.getValue().receiveServerMsg(System.getProperty("quota") + " " + new Date().toString());
+                            long used_mem = readFileInt("/sys/fs/cgroup/memory/memory.usage_in_bytes");
+                            long limit_mem = readFileInt("/sys/fs/cgroup/memory/memory.limit_in_bytes");
+                            long free_mem = limit_mem - used_mem;
+                            entry.getValue().receiveServerMsg(System.getProperty("quota") + ":" + Long.toString(free_mem) + ":" + Long.toString(limit_mem));
                         } catch (Throwable t1) {
                             listeners.remove(entry.getKey());
                         }
