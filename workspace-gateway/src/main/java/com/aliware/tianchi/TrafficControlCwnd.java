67d0276ee39fbdfc6fcdeb74af124eab5c4addf6
zbw0046 <zbw0046@gmail.com>
2019-07-15 15:13:17 +0800

    增加cwnd流控窗口

---
+++ workspace-gateway/src/main/java/com/aliware/tianchi/TrafficControlCwnd.java
@@ -0,0 +1,43 @@
+package com.aliware.tianchi;
+
+public class TrafficControlCwnd {
+    private int cwnd;
+    private int ssthresh;
+    private int timeoutCnt;
+    private int timeoutThreshold;
+
+    TrafficControlCwnd(int initCwnd, int timeoutThreshold, int ssthresh){
+        this.cwnd = initCwnd;
+        this.ssthresh = ssthresh;
+        this.timeoutThreshold = timeoutThreshold;
+    }
+
+    public int getCurrentCwnd(boolean lastSuccess) throws Exception{
+        int ret = this.refreshCwnd(lastSuccess);
+        if(ret != 0){
+            throw new Exception(String.format("refresh cwnd return %d\n", ret));
+        }
+        return this.cwnd;
+    }
+
+    private int refreshCwnd(boolean lastSuccess){
+        int nextCwnd = cwnd;
+        if(lastSuccess){
+            if((nextCwnd << 1) > this.ssthresh){
+                nextCwnd += 1;
+            } else {
+                nextCwnd = nextCwnd << 1;
+            }
+            this.timeoutCnt = 0;
+        } else {
+            this.timeoutCnt += 1;
+            if(this.timeoutCnt > this.timeoutThreshold){
+                this.ssthresh = nextCwnd >> 1;
+                nextCwnd = this.ssthresh;
+            }
+        }
+        this.cwnd = nextCwnd;
+        System.out.printf("cwnd=%d,ssthresh=%d,lastSuccess=%b\t", this.cwnd, this.ssthresh, lastSuccess);
+        return 0;
+    }
+}
---
+++ workspace-gateway/src/main/java/com/aliware/tianchi/TrafficControlCwndTest.java
@@ -0,0 +1,23 @@
+package com.aliware.tianchi;
+
+import java.util.Random;
+
+public class TrafficControlCwndTest {
+    public static void main(String[] args){
+        TrafficControlCwnd cwndGetter = new TrafficControlCwnd(10, 2, 30);
+        for(int i=0;i<100;i++){
+            Random r = new Random();
+            double u = r.nextDouble();
+            boolean lastSuccess = true;
+            if(u > 0.6){
+                lastSuccess = false;
+            }
+            try {
+                int currentCwnd = cwndGetter.getCurrentCwnd(lastSuccess);
+                System.out.printf("current cwnd: %d\n", currentCwnd);
+            } catch (Exception e){
+                System.out.printf("exception");
+            }
+        }
+    }
+}
